e10f7f35e9c42f77f1cca02396114df03dd44bae
balaganesh-v <vbalaganesh2007@gmail.com>
2025-08-31 19:41:05 +0530

    Add the Frontend Project Structure

---
+++ frontend_service/src/pages/index.js
@@ -0,0 +1,10 @@
+// pages/index.js
+export default function Home() {
+  return (
+    <div className="flex items-center justify-center h-screen">
+      <h1 className="text-4xl font-bold text-blue-600">
+        Welcome to Next.js with Tailwind
+      </h1>
+    </div>
+  );
+}
